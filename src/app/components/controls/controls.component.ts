615794007c0bf2506e565add299c019f64287db8
Illia.Kaspiarovich <Illia.Kaspiarovich@easyfundraising.org.uk>
2023-07-31 13:23:34 +0200

    adds some UX improvements

--- src/app/components/controls/controls.component.ts
+++ src/app/components/controls/controls.component.ts
@@ -1,17 +1,28 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Itodo } from '@src/app/models/todo';
 
 @Component({
   selector: 'app-controls',
   templateUrl: './controls.component.html'
 })
-export class ControlsComponent {
+export class ControlsComponent implements AfterViewInit {
   newTodoText = '';
 
+  @ViewChild('todoInput') todoInput: ElementRef;
   @Output() createTodoEvent = new EventEmitter<Itodo['text']>()
+
+  ngAfterViewInit() {
+    this.todoInput.nativeElement.focus();
+  }
 
   onCreateClick() {
     this.createTodoEvent.emit(this.newTodoText)
     this.newTodoText = ''
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.onCreateClick()
+    }
   }
 }
